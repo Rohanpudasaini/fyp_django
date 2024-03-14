9b1892d27b95befe2eb3528c172a5be3addc02b5
Rohan <pudasainirohan0@gmail.com>
2024-03-14 23:31:13 +0545

    feat(cart): Added cart and cart quantity

--- cart/cart.py
+++ cart/cart.py
@@ -2,22 +2,28 @@ class Cart():
  
     def __init__(self, request) -> None:
         self.session = request.session
+        self.request = request
         cart = self.session.get('session_key')
         
-        if 'session.key' not in request.session:
+        if 'session_key' not in request.session:
+            # cart = self.session['session_key'] = {}
             cart = self.session['session_key'] = {}
 
         self.cart = cart
     
     def add(self, product):
         product_id = str(product.id)
-        print("Hello")
         if product_id in self.cart:
             pass
         else:
-            if product.is_sale:
-                self.cart[product_id] = {'price':str(product.sale_price)}
-            else:
-                self.cart[product_id] = {'price':str(product.price)}
+            # if product.is_sale:
+                # self.cart[product_id] = {'price':str(product.sale_price)}
+            # else:
+                # self.cart[product_id] = {'price':str(product.price)}
+            # self.cart[product_id] = {'price':str(product.price)}
+            self.cart[product_id] = {'price': str(product.price)}
         
         self.session.modified = True
+        
+    def __len__(self):
+        return len(self.cart)
